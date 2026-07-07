23cbc667049889087720f280bbc42490ce9a8c00
Dhruv Tiwari <dhruvtiwari1402@gmail.com>
2026-07-07 20:55:01 +0530

    Time: 9 ms (43.52%), Space: 41.6 MB (96.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,7 @@
 class Solution {
     public int fib(int n) {
-        return f(n);
-
-    }
-    public int f(int n){
         if(n<=1) return n;
-        return f(n-1)+f(n-2);
+        return fib(n-1)+fib(n-2);
+
     }
 }
